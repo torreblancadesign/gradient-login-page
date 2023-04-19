2e8415919a67d371c3ba39d4921209529ea6664f
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-19 15:59:02 -0700

    Add components/component.js

---
+++ components/component.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import styles from '../styles/style.module.css';
+
+const Login = () => {
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.loginForm}>
+        <div className={styles.loginFields}>
+          <input type="text" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
+        </div>
+        <button type="submit" className={styles.loginButton}>Login</button>
+      </div>
+      <div className={styles.footer}>
+        Simple Footer
+      </div>
+    </div>
+  )
+}
+
+export default Login;
